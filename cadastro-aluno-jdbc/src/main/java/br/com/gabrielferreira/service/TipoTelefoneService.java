4f2f1833922de3c4be969a8cf885580752845bd4
gabrielferreira <gabrielferreira@magnasistemas.com.br>
2022-08-01 20:50:15 -0300

    Teste unitário para o tipo telefone

--- cadastro-aluno-jdbc/src/main/java/br/com/gabrielferreira/service/TipoTelefoneService.java
+++ cadastro-aluno-jdbc/src/main/java/br/com/gabrielferreira/service/TipoTelefoneService.java
@@ -2,19 +2,17 @@ package br.com.gabrielferreira.service;
 import br.com.gabrielferreira.dao.TipoTelefoneDAO;
 import br.com.gabrielferreira.exceptions.RegraDeNegocioException;
 import br.com.gabrielferreira.modelo.TipoTelefone;
+import lombok.RequiredArgsConstructor;
 
 import java.io.Serializable;
 import java.util.List;
 
+@RequiredArgsConstructor
 public class TipoTelefoneService implements Serializable {
 
     private static final long serialVersionUID = -4873911623755365754L;
 
     private final TipoTelefoneDAO tipoTelefoneDAO;
-
-    public TipoTelefoneService(){
-        tipoTelefoneDAO = new TipoTelefoneDAO();
-    }
 
     public List<TipoTelefone> listaDeTipoTelefones(){
         return tipoTelefoneDAO.listaDeTipoTelefones();
@@ -22,10 +20,14 @@ public class TipoTelefoneService implements Serializable {
 
     public TipoTelefone buscarTipoTelefonePorId(Long id){
         TipoTelefone tipoTelefone = tipoTelefoneDAO.buscarPorId(id);
-        if(tipoTelefone.getId() == null){
+        if(!buscarTipoTelefone(tipoTelefone)){
             throw new RegraDeNegocioException("Tipo de telefone não encontrado.");
         }
         return tipoTelefone;
+    }
+
+    private boolean buscarTipoTelefone(TipoTelefone tipoTelefone){
+        return tipoTelefone != null && tipoTelefone.getId() != null;
     }
 
 }

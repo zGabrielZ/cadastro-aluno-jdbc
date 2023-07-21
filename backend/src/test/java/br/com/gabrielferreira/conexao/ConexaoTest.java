69a1873a35538fa3aab668f5f55b2545be9a9f85
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-07-20 21:18:45 -0300

    Teste da classe conexao db

---
+++ backend/src/test/java/br/com/gabrielferreira/conexao/ConexaoTest.java
@@ -0,0 +1,20 @@
+package br.com.gabrielferreira.conexao;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class ConexaoTest {
+
+    @Test
+    @DisplayName("Deve não conectar no banco de dados quando informar ambiente incorreto")
+    void deveNaoConectarComBancoDeDadosAmbienteIncorreto(){
+        try {
+            new ConexaoBD("AMBIENTE_INCORRETO");
+            fail("Deveria ter lançado a exceção do ambiente incorreto");
+        } catch (Exception e){
+            assertTrue(e.getMessage().contains("Ocorreu um erro ao conectar no banco de dados"));
+        }
+    }
+}
